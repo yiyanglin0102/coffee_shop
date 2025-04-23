7e01bc6cdba3519ad0925136281cd5d9015f6524
yiyanglin0102 <yiyanglin0102@gmail.com>
2025-04-23 00:36:58 -0400

    Optimize .gitignore for Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "coffeeshop664b6379": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
